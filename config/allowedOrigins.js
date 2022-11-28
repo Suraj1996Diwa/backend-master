334485803a05a5d5bbc5f91a5ef6616aa6097366
[Suraj1996Diwa] <101166221+Suraj1996Diwa@users.noreply.github.com>
2022-11-28 15:35:50 +0530

    first commit

---
+++ config/allowedOrigins.js
@@ -0,0 +1,8 @@
+const allowedOrigins = [
+    'htVtp://localhost:3600',
+    'htVtp://localhost:3500',
+    'htVtp://localhost:3100'
+    
+]
+
+module.exports = allowedOrigins
---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const path = require('path')
+const dotenv = require('dotenv')
+dotenv.config()
+const app = express()
+const PORT = process.env.PORT || 3500
+
+//database 
+const connectDB = require('./config/dbConn')
+const mongoose = require('mongoose')
+
+connectDB()
+
+//middleware
+const { logger } = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
+
+const corsOptions = require('./config/corsOptions')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+
+
+
+
+//middleware
+app.use(logger)
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(cookieParser())
+
+
+
+
+//static files
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+
+
+
+//routes
+app.use('/', require('./routes/root'))
+
+app.all('*', (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({ message: '404 Not Found' })
+    } else {
+        res.type('txt').send('404 Not Found')
+    }
+})
+
+
+app.use(errorHandler)
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB')
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+})
+
+mongoose.connection.on('error', err => {
+    console.log(err)
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
+})
